e6d2107a3a9f2338dd8f300522809af65cd606af
Steven Chen <ysteven.chen2@gmail.com>
2018-05-08 03:28:47 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,14 @@
 def reformat_languages(languages)
   # your code here
+  reformatting={}
+  languages.each do |style,lang|
+    lang.each do |name,type|
+      if !reformatting[name]
+        reformatting[name] = type
+        reformatting[name][:style]=[]
+      end
+      reformatting[name][:style] << style
+    end
+      end
+      reformatting
 end
